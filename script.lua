f2df2f5227cbae47fd96bcb4fc7bec938a6281f2
Bemplia <145816318+Bemplia@users.noreply.github.com>
2024-07-06 19:06:47 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,6 +1,5 @@
 -- Variables
 local player = game.Players.LocalPlayer
-local playergui = player.PlayerGui.Main
 local character = player.Character
 local uis = game:GetService("UserInputService")
 local jump = 50
@@ -25,120 +24,120 @@ end)
 
 MainSection:NewToggle("Hide interface", "", function(state)
     if state then
-        playergui.Main.Left.Visible = false
-        playergui.Main.Right.Visible = false
+        game.Players.LocalPlayer.Main.Main.Left.Visible = false
+        game.Players.LocalPlayer.Main.Main.Right.Visible = false
     else
-        playergui.Main.Left.Visible = true
-        playergui.Main.Right.Visible = true
+        game.Players.LocalPlayer.Main.Main.Left.Visible = true
+        game.Players.LocalPlayer.Main.Main.Right.Visible = true
     end
 end)
 
 MainSection:NewButton("Auto Answer", "", function()
         while task.wait(0.3) do
-            if playergui.Question.QuestionTxt.Text == "Name a type of transport" then
-                playergui.Answer.LeftBg.textBox.Text = "Airplane"
+            if game.Players.LocalPlayer.Main.Question.QuestionTxt.Text == "Name a type of transport" then
+                game.Players.LocalPlayer.Main.Answer.LeftBg.textBox.Text = "Airplane"
             end
-            if playergui.Question.QuestionTxt.Text == "Name an animal that can fly" then
-                playergui.Answer.LeftBg.textBox.Text = "Honeybee"
+            if game.Players.LocalPlayer.Main.Question.QuestionTxt.Text == "Name an animal that can fly" then
+                game.Players.LocalPlayer.Main.Answer.LeftBg.textBox.Text = "Honeybee"
             end
-            if playergui.Question.QuestionTxt.Text == "Name a musical instrument" then
-                playergui.Answer.LeftBg.textBox.Text = "Electric guitar"
+            if game.Players.LocalPlayer.Main.Question.QuestionTxt.Text == "Name a musical instrument" then
+                game.Players.LocalPlayer.Main.Answer.LeftBg.textBox.Text = "Electric guitar"
             end
-            if playergui.Question.QuestionTxt.Text == "Name a popular fruit" then
-                playergui.Answer.LeftBg.textBox.Text = "Pomegranate"
+            if game.Players.LocalPlayer.Main.Question.QuestionTxt.Text == "Name a popular fruit" then
+                game.Players.LocalPlayer.Main.Answer.LeftBg.textBox.Text = "Pomegranate"
             end
-            if playergui.Question.QuestionTxt.Text == "Name any month that has 31 days" then
-                playergui.Answer.LeftBg.textBox.Text = "December"
+            if game.players.localplayer.main.Question.QuestionTxt.Text == "Name any month that has 31 days" then
+                game.Players.LocalPlayer.Main.Answer.LeftBg.textBox.Text = "December"
             end
-            if playergui.Question.QuestionTxt.Text == "Name a popular game on Roblox" then
-                playergui.Answer.LeftBg.textBox.Text = "Murder mystery"
+            if game.Players.LocalPlayer.Main.Question.QuestionTxt.Text == "Name a popular game on Roblox" then
+                game.Players.LocalPlayer.Main.Answer.LeftBg.textBox.Text = "Murder mystery"
             end
-            if playergui.Question.QuestionTxt.Text == "Name a popular electronic device" then
-                playergui.Answer.LeftBg.textBox.Text = "Telephone"
+            if game.Players.LocalPlayer.Main.Question.QuestionTxt.Text == "Name a popular electronic device" then
+                game.Players.LocalPlayer.Main.Answer.LeftBg.textBox.Text = "Telephone"
             end
-            if playergui.Question.QuestionTxt.Text == "Name something you do in your sleep" then
-                playergui.Answer.LeftBg.textBox.Text = "Dream"
+            if game.Players.LocalPlayer.Main.Question.QuestionTxt.Text == "Name something you do in your sleep" then
+                game.Players.LocalPlayer.Main.Answer.LeftBg.textBox.Text = "Dream"
             end
-            if playergui.Question.QuestionTxt.Text == "Name a famous sport that is played in teams" then
-                playergui.Answer.LeftBg.textBox.Text = "Basketball"
+            if game.Players.LocalPlayer.Main.Question.QuestionTxt.Text == "Name a famous sport that is played in teams" then
+                game.Players.LocalPlayer.Main.Answer.LeftBg.textBox.Text = "Basketball"
             end
-            if playergui.Question.QuestionTxt.Text == "Name a red fruit" then
-                playergui.Answer.LeftBg.textBox.Text = "Pomegranate"
+            if game.Players.LocalPlayer.Main.Question.QuestionTxt.Text == "Name a red fruit" then
+                game.Players.LocalPlayer.Main.Answer.LeftBg.textBox.Text = "Pomegranate"
             end
-            if playergui.Question.QuestionTxt.Text == "Name one of the seven colors of the rainbow" then
-                playergui.Answer.LeftBg.textBox.Text = "Purple"
+            if game.Players.LocalPlayer.Main.Question.QuestionTxt.Text == "Name one of the seven colors of the rainbow" then
+                game.Players.LocalPlayer.Main.Answer.LeftBg.textBox.Text = "Purple"
             end
-            if playergui.Question.QuestionTxt.Text == "Name a popular vegetable" then
-                playergui.Answer.LeftBg.textBox.Text = "Carrot"
+            if game.Players.LocalPlayer.Main.Question.QuestionTxt.Text == "Name a popular vegetable" then
+                game.Players.LocalPlayer.Main.Answer.LeftBg.textBox.Text = "Carrot"
             end
-            if playergui.Question.QuestionTxt.Text == "Name a type of weather" then
-                playergui.Answer.LeftBg.textBox.Text = "Thunder"
+            if game.Players.LocalPlayer.Main.Question.QuestionTxt.Text == "Name a type of weather" then
+                game.Players.LocalPlayer.Main.Answer.LeftBg.textBox.Text = "Thunder"
             end
-            if playergui.Question.QuestionTxt.Text == "Name one of the world's most popular car colors" then
-                playergui.Answer.LeftBg.textBox.Text = "Orange"
+            if game.Players.LocalPlayer.Main.Question.QuestionTxt.Text == "Name one of the world's most popular car colors" then
+                game.Players.LocalPlayer.Main.Answer.LeftBg.textBox.Text = "Orange"
             end
-            if playergui.Question.QuestionTxt.Text == "Name one of the four seasons" then
-                playergui.Answer.LeftBg.textBox.Text = "Summer"
+            if game.Players.LocalPlayer.Main.Question.QuestionTxt.Text == "Name one of the four seasons" then
+                game.Players.LocalPlayer.Main.Answer.LeftBg.textBox.Text = "Summer"
             end
-            if playergui.Question.QuestionTxt.Text == "Name any part of the head" then
-                playergui.Answer.LeftBg.textBox.Text = "Forehead"
+            if game.Players.LocalPlayer.Main.Question.QuestionTxt.Text == "Name any part of the head" then
+                game.Players.LocalPlayer.Main.Answer.LeftBg.textBox.Text = "Forehead"
             end
-            if playergui.Question.QuestionTxt.Text == "Name any natural hair color" then
-                playergui.Answer.LeftBg.textBox.Text = "Brown"
+            if game.Players.LocalPlayer.Main.Question.QuestionTxt.Text == "Name any natural hair color" then
+                game.Players.LocalPlayer.Main.Answer.LeftBg.textBox.Text = "Brown"
             end
-            if playergui.Question.QuestionTxt.Text == "Name something you find on pizza" then
-                playergui.Answer.LeftBg.textBox.Text = "Pepporoni"
+            if game.Players.LocalPlayer.Main.Question.QuestionTxt.Text == "Name something you find on pizza" then
+                game.Players.LocalPlayer.Main.Answer.LeftBg.textBox.Text = "Pepporoni"
             end
-            if playergui.Question.QuestionTxt.Text == "Name something you eat with" then
-                playergui.Answer.LeftBg.textBox.Text = "Spoon"
+            if game.Players.LocalPlayer.Main.Question.QuestionTxt.Text == "Name something you eat with" then
+                game.Players.LocalPlayer.Main.Answer.LeftBg.textBox.Text = "Spoon"
             end
-            if playergui.Question.QuestionTxt.Text == "Name a topping that is usually in a hamburger" then
-                playergui.Answer.LeftBg.textBox.Text = "Mayonnaise"
+            if game.Players.LocalPlayer.Main.Question.QuestionTxt.Text == "Name a topping that is usually in a hamburger" then
+                game.Players.LocalPlayer.Main.Answer.LeftBg.textBox.Text = "Mayonnaise"
             end
-            if playergui.Question.QuestionTxt.Text == "Name something people wear" then
-                playergui.Answer.LeftBg.textBox.Text = "Sweatshirt"
+            if game.Players.LocalPlayer.Main.Question.QuestionTxt.Text == "Name something people wear" then
+                game.Players.LocalPlayer.Main.Answer.LeftBg.textBox.Text = "Sweatshirt"
             end
-            if playergui.Question.QuestionTxt.Text == "Name something you do at school" then
-                playergui.Answer.LeftBg.textBox.Text = "Learning"
+            if game.Players.LocalPlayer.Main.Question.QuestionTxt.Text == "Name something you do at school" then
+                game.Players.LocalPlayer.Main.Answer.LeftBg.textBox.Text = "Learning"
             end
-            if playergui.Question.QuestionTxt.Text == "Name a day of the week" then
-                playergui.Answer.LeftBg.textBox.Text = "Wednesday"
+            if game.Players.LocalPlayer.Main.Question.QuestionTxt.Text == "Name a day of the week" then
+                game.Players.LocalPlayer.Main.Answer.LeftBg.textBox.Text = "Wednesday"
             end
-            if playergui.Question.QuestionTxt.Text == "Name a social media app" then
-                playergui.Answer.LeftBg.textBox.Text = "Instagram"
+            if game.Players.LocalPlayer.Main.Question.QuestionTxt.Text == "Name a social media app" then
+                game.Players.LocalPlayer.Main.Answer.LeftBg.textBox.Text = "Instagram"
             end
-            if playergui.Question.QuestionTxt.Text == "Name any capital city in Europe" then
-                playergui.Answer.LeftBg.textBox.Text = "Amsterdam"
+            if game.Players.LocalPlayer.Main.Question.QuestionTxt.Text == "Name any capital city in Europe" then
+                game.Players.LocalPlayer.Main.Answer.LeftBg.textBox.Text = "Amsterdam"
             end
-            if playergui.Question.QuestionTxt.Text == "Name a country that starts with the letter A" then
-                playergui.Answer.LeftBg.textBox.Text = "Australia"
+            if game.Players.LocalPlayer.Main.Question.QuestionTxt.Text == "Name a country that starts with the letter A" then
+                game.Players.LocalPlayer.Main.Answer.LeftBg.textBox.Text = "Australia"
             end
-            if playergui.Question.QuestionTxt.Text == "What is something you eat with your hands" then
-                playergui.Answer.LeftBg.textBox.Text = "Chickennugget"
+            if game.Players.LocalPlayer.Main.Question.QuestionTxt.Text == "What is something you eat with your hands" then
+                game.Players.LocalPlayer.Main.Answer.LeftBg.textBox.Text = "Chickennugget"
             end
-            if playergui.Question.QuestionTxt.Text == "Name a red fruit" then
-                playergui.Answer.LeftBg.textBox.Text = "Pomegranate"
+            if game.Players.LocalPlayer.Main.Question.QuestionTxt.Text == "Name a red fruit" then
+                game.Players.LocalPlayer.Main.Answer.LeftBg.textBox.Text = "Pomegranate"
             end
-            if playergui.Question.QuestionTxt.Text == "Name a planet that is part of our solar system" then
-                playergui.Answer.LeftBg.textBox.Text = "Jupiter"
+            if game.Players.LocalPlayer.Main.Question.QuestionTxt.Text == "Name a planet that is part of our solar system" then
+                game.Players.LocalPlayer.Main.Answer.LeftBg.textBox.Text = "Jupiter"
             end
-            if playergui.Question.QuestionTxt.Text == "Name one of the fastest animals" then
-                playergui.Answer.LeftBg.textBox.Text = "Leopard"
+            if game.Players.LocalPlayer.Main.Question.QuestionTxt.Text == "Name one of the fastest animals" then
+                game.Players.LocalPlayer.Main.Answer.LeftBg.textBox.Text = "Leopard"
             end
-            if playergui.Question.QuestionTxt.Text == "Name a popular Superhero" then
-                playergui.Answer.LeftBg.textBox.Text = "Spiderman"
+            if game.Players.LocalPlayer.Main.Question.QuestionTxt.Text == "Name a popular Superhero" then
+                game.Players.LocalPlayer.Main.Answer.LeftBg.textBox.Text = "Spiderman"
             end
-            if playergui.Question.QuestionTxt.Text == "What is something you can sit on" then
-                playergui.Answer.LeftBg.textBox.Text = "Chair"
+            if game.Players.LocalPlayer.Main.Question.QuestionTxt.Text == "What is something you can sit on" then
+                game.Players.LocalPlayer.Main.Answer.LeftBg.textBox.Text = "Chair"
             end
-            if playergui.Question.QuestionTxt.Text == "Name a food that starts with the letter P" then
-                playergui.Answer.LeftBg.textBox.Text = "Pasta"
+            if game.Players.LocalPlayer.Main.Question.QuestionTxt.Text == "Name a food that starts with the letter P" then
+                game.Players.LocalPlayer.Main.Answer.LeftBg.textBox.Text = "Pasta"
             end
-            if playergui.Question.QuestionTxt.Text == "Name something people wear" then
-                playergui.Answer.LeftBg.textBox.Text = "Clothing"
+            if game.Players.LocalPlayer.Main.Question.QuestionTxt.Text == "Name something people wear" then
+                game.Players.LocalPlayer.Main.Answer.LeftBg.textBox.Text = "Clothing"
             end
-            if playergui.Question.QuestionTxt.Text == "Name an animal that walks slowly" then
-                playergui.Answer.LeftBg.textBox.Text = "Turtle"
+            if game.Players.LocalPlayer.Main.Question.QuestionTxt.Text == "Name an animal that walks slowly" then
+                game.Players.LocalPlayer.Main.Answer.LeftBg.textBox.Text = "Turtle"
             end
         end
 end)
